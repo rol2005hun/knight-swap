3091db871f9c44daf739351a3c53f36475c8a0b6
Ranyák Roland Adalbert <rol2005hun@mailbox.unideb.hu>
2025-06-05 22:39:48 +0300

    feat: add Main.java and package-info.java

---
+++ src/main/java/knightswap/Main.java
@@ -0,0 +1,9 @@
+package knightswap;
+
+import org.tinylog.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger.info("The application has been started.");
+    }
+}
---
+++ src/main/java/knightswap/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Implements the Knight Swap Chess game.
+ */
+
+package knightswap;
